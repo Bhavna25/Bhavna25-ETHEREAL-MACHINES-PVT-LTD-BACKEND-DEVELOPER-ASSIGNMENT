0299edd35b683b5289192d55fc7adf36ad0949bf
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2024-10-01 19:55:43 +0530

    Create UserController.java

---
+++ Part2/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,18 @@
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id).orElse(null));
+    }
+
+    // Additional CRUD operations
+}
