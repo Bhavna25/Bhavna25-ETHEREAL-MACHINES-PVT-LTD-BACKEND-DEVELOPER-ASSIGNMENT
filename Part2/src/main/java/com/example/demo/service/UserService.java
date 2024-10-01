be85123678adcdb6cf51e0eba787d0284132d065
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2024-10-01 20:10:38 +0530

    Update UserService.java

--- Part2/src/main/java/com/example/demo/service/UserService.java
+++ Part2/src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,12 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public User saveUser(User user) {
-        // Encode password and save
         return userRepository.save(user);
     }
 
