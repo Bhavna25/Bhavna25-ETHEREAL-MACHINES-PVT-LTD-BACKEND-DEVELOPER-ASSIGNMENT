13e1d573f949964a7c5bed22dc75a1ac91ba75be
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2024-10-01 19:56:46 +0530

    Create User.java

---
+++ Part2/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,11 @@
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role; // e.g., ROLE_USER, ROLE_ADMIN
+    // Getters and Setters
+}
